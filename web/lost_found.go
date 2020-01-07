ac98705d53aeb8b7f5b521da0e78870cf1273760
SueHsia <skj865@outlook.com>
2020-01-07 13:21:22 +0800

    添加token验证功能

--- web/lost_found.go
+++ web/lost_found.go
@@ -64,24 +64,24 @@ func index(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	r.ParseForm()
-	res_page := r.URL.Query()["page"][0]
-	if res_page == "" {
-		res_page = "1"
+	resPage := r.URL.Query()["page"][0]
+	if resPage == "" {
+		resPage = "1"
 	}
 	dict := dic{}
-	tem_goods := Goodsinfo{}
-	page, _ := strconv.Atoi(res_page)
+	temGoods := Goodsinfo{}
+	page, _ := strconv.Atoi(resPage)
 	rows, err := db.Query("select * from lost_goods where status = ? limit ?,?", 1, (int(page)-1)*8, int(page)*8)
 	errorHandle(err, w)
 	db.QueryRow("select count(*) from lost_goods where status = ?", 1).Scan(&dict.Total_count)
 	dict.Total_page = int(math.Ceil(float64(dict.Total_count) / 8.0))
 	errorHandle(err, w)
 	for rows.Next() {
-		rows.Scan(&tem_goods.Goodsid, &tem_goods.Goodsname, &tem_goods.Address, &tem_goods.Pic, &tem_goods.Phone, &tem_goods.Des, &tem_goods.Userid, &tem_goods.Create_time, &tem_goods.Update_time, &tem_goods.View_count, &tem_goods.Status, &tem_goods.Is_return)
-		dict.Data = append(dict.Data, tem_goods)
+		rows.Scan(&temGoods.Goodsid, &temGoods.Goodsname, &temGoods.Address, &temGoods.Pic, &temGoods.Phone, &temGoods.Des, &temGoods.Userid, &temGoods.Create_time, &temGoods.Update_time, &temGoods.View_count, &temGoods.Status, &temGoods.Is_return)
+		dict.Data = append(dict.Data, temGoods)
 	}
-	dict_json, _ := json.Marshal(dict)
-	result := string(dict_json)
+	dictJSON, _ := json.Marshal(dict)
+	result := string(dictJSON)
 	fmt.Fprint(w, result)
 }
 
@@ -100,31 +100,31 @@ func login(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	dict := dic{}
-	tem_data := Userinfo{}
+	temData := Userinfo{}
 	var result string
 	if r.Method == "POST" {
 		r.ParseForm()
 		username := r.Form["username"][0]
 		password := r.Form["password"][0]
 		rows := db.QueryRow("select * from pi_user where username = ?", username)
-		rows.Scan(&tem_data.Userid, &tem_data.Username, &tem_data.Password, &tem_data.Create_time)
-		if tem_data.Username == "" {
+		rows.Scan(&temData.Userid, &temData.Username, &temData.Password, &temData.Create_time)
+		if temData.Username == "" {
 			dict.Msg = "用户名或密码错误"
 			dict.Flag = 0
 		} else {
-			new_pass := md5V(password)
-			if new_pass != tem_data.Password {
+			newPass := md5V(password)
+			if newPass != temData.Password {
 				dict.Msg = "用户名或密码错误"
 				dict.Flag = 0
 			} else {
-				dict.Token = enbase([]byte(username + string(time.Now().Unix())))
 				dict.Flag = 1
 			}
-			dict.Username = tem_data.Username
-			dict.Userid = tem_data.Userid
+			dict.Username = temData.Username
+			dict.Userid = temData.Userid
+			dict.Token = enbase(temData.Username + "," + strconv.Itoa(temData.Userid) + "," + strconv.FormatInt(time.Now().Unix(), 10))
 		}
-		dict_json, _ := json.Marshal(dict)
-		result = string(dict_json)
+		dictJSON, _ := json.Marshal(dict)
+		result = string(dictJSON)
 	}
 	fmt.Fprintf(w, string(result))
 }
@@ -133,7 +133,7 @@ func register(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	dict := dic{}
-	tem_data := Userinfo{}
+	temData := Userinfo{}
 	var result string
 	if r.Method == "POST" {
 		r.ParseForm()
@@ -141,26 +141,26 @@ func register(w http.ResponseWriter, r *http.Request) {
 		password := strings.Replace(r.Form["password"][0], " ", "", -1)
 		repassword := strings.Replace(r.Form["repassword"][0], " ", "", -1)
 		rows := db.QueryRow("select * from pi_user where username = ?", username)
-		rows.Scan(&tem_data.Userid, &tem_data.Username, &tem_data.Password, &tem_data.Create_time)
+		rows.Scan(&temData.Userid, &temData.Username, &temData.Password, &temData.Create_time)
 		if password != repassword {
 			dict.Msg = "密码不一致，注册失败！"
 			dict.Flag = 0
-		} else if tem_data.Username != "" {
+		} else if temData.Username != "" {
 			dict.Msg = "用户已存在，注册失败"
 			dict.Flag = 0
 		} else {
 			password = md5V(password)
 			tm := time.Unix(time.Now().Unix(), 0)
-			create_time := tm.Format("2006-01-02 15:04:05")
+			createTime := tm.Format("2006-01-02 15:04:05")
 			stmt, err := db.Prepare("INSERT pi_user SET username=?,password=?,create_time=?")
 			errorHandle(err, w)
-			stmt.Exec(username, password, create_time)
+			stmt.Exec(username, password, createTime)
 			dict.Msg = "注册成功！"
 			dict.Flag = 1
 		}
-		dict_json, err := json.Marshal(dict)
+		dictJSON, err := json.Marshal(dict)
 		errorHandle(err, w)
-		result = string(dict_json)
+		result = string(dictJSON)
 	}
 	fmt.Fprintf(w, result)
 }
@@ -170,21 +170,37 @@ func publish(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "multipart/form-data")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	dict := dic{}
-	tem_user := Userinfo{}
-	tem_goods := Goodsinfo{}
+	var result string
+	temUser := Userinfo{}
+	temGoods := Goodsinfo{}
 	var ext string
 	r.ParseForm()
-	uploadFile, handle, err := r.FormFile("file")
-	errorHandle(err, w)
-	var result string
-	if r.Method == "POST" {
+	token := r.Form["token"][0]
+	curUsername, curUserid, isValid := validate(token)
+	if !isValid {
+		dict.Msg = "登录失效，请重新登录"
+		dict.Flag = 0
+		dictJSON, err := json.Marshal(dict)
+		errorHandle(err, w)
+		result = string(dictJSON)
+		fmt.Fprintf(w, result)
+		return
+	}
+	if r.Method == "OPTIONS" {
+		fmt.Fprintf(w, "")
+		return
+	} else if r.Method == "GET" {
+		fmt.Fprintf(w, "")
+	} else if r.Method == "POST" {
+		uploadFile, handle, err := r.FormFile("file")
+		errorHandle(err, w)
 		tm := time.Unix(time.Now().Unix(), 0)
-		tem_goods.Address = r.Form["address"][0]
-		tem_user.Username = r.Form["username"][0]
-		tem_goods.Goodsname = r.Form["name"][0]
-		tem_goods.Userid, _ = strconv.Atoi(r.Form["userid"][0])
-		tem_goods.Phone = r.Form["phone"][0]
-		tem_goods.Des = r.Form["des"][0]
+		temGoods.Address = r.Form["address"][0]
+		temUser.Username = curUsername
+		temGoods.Goodsname = r.Form["name"][0]
+		temGoods.Userid = curUserid
+		temGoods.Phone = r.Form["phone"][0]
+		temGoods.Des = r.Form["des"][0]
 		name := strings.Split(handle.Filename, ".")
 		ext = strings.ToLower(name[len(name)-1])
 		fileDir := fmt.Sprintf("./media/file/%v/", tm.Format("2006-01-02"))
@@ -197,18 +213,19 @@ func publish(w http.ResponseWriter, r *http.Request) {
 		io.Copy(saveFile, uploadFile)
 		defer uploadFile.Close()
 		defer saveFile.Close()
-		tem_goods.Pic = filename[1:]
+		temGoods.Pic = filename[1:]
 		stmt, err := db.Prepare("INSERT lost_goods SET name=?,address=?,pic=?,phone=?,des=?,user_id=?,create_time=?,update_time=?,view_count=?,status=?,is_return=?")
 		errorHandle(err, w)
-		stmt.Exec(tem_goods.Goodsname, tem_goods.Address, tem_goods.Pic, tem_goods.Phone, tem_goods.Des, tem_goods.Userid, tm.Format("2006-01-02 15:04:05"), tm.Format("2006-01-02 15:04:05"), 0, 1, 0)
+		stmt.Exec(temGoods.Goodsname, temGoods.Address, temGoods.Pic, temGoods.Phone, temGoods.Des, temGoods.Userid, tm.Format("2006-01-02 15:04:05"), tm.Format("2006-01-02 15:04:05"), 0, 1, 0)
 		dict.Msg = "发布成功"
 		dict.Flag = 1
 		// 上传图片成功
-		dict_json, err := json.Marshal(dict)
+		dictJSON, err := json.Marshal(dict)
 		errorHandle(err, w)
-		result = string(dict_json)
+		result = string(dictJSON)
+		fmt.Fprintf(w, result)
+		return
 	}
-	fmt.Fprintf(w, result)
 }
 
 func detail(w http.ResponseWriter, r *http.Request) {
@@ -216,65 +233,36 @@ func detail(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	r.ParseForm()
-	if r.Method == "GET" {
-		tem_goods := Goodsinfo{}
-		goods_id, _ := strconv.Atoi(r.URL.Query()["goods_id"][0])
-		rows := db.QueryRow("select * from lost_goods where id = ?", goods_id)
-		rows.Scan(&tem_goods.Goodsid, &tem_goods.Goodsname, &tem_goods.Address, &tem_goods.Pic, &tem_goods.Phone, &tem_goods.Des, &tem_goods.Userid, &tem_goods.Create_time, &tem_goods.Update_time, &tem_goods.View_count, &tem_goods.Status, &tem_goods.Is_return)
+	temGoods := Goodsinfo{}
+	dict := dic{}
+	var token string
+	var goodsID string
+	if r.Method == "OPTIONS" {
+		fmt.Fprintf(w, "")
+		return
+	} else if r.Method == "GET" {
+		token = r.Form["token"][0]
+		goodsID = r.Form["goods_id"][0]
+		_, _, isValid := validate(token)
+		if !isValid {
+			dict.Msg = "登录失效，请重新登录"
+			dict.Flag = 0
+			dictJSON, err := json.Marshal(dict)
+			errorHandle(err, w)
+			result := string(dictJSON)
+			fmt.Fprintf(w, result)
+			return
+		}
+		rows := db.QueryRow("select * from lost_goods where id = ?", goodsID)
+		rows.Scan(&temGoods.Goodsid, &temGoods.Goodsname, &temGoods.Address, &temGoods.Pic, &temGoods.Phone, &temGoods.Des, &temGoods.Userid, &temGoods.Create_time, &temGoods.Update_time, &temGoods.View_count, &temGoods.Status, &temGoods.Is_return)
 		stmt, err := db.Prepare("update lost_goods set view_count=? where id=?")
 		errorHandle(err, w)
-		stmt.Exec(tem_goods.View_count+1, tem_goods.Goodsid)
-		dict := dic{}
-		dict.Data = append(dict.Data, tem_goods)
-		dict_json, err := json.Marshal(dict)
+		count := temGoods.View_count + 1
+		stmt.Exec(count, temGoods.Goodsid)
+		dict.Data = append(dict.Data, temGoods)
+		dictJSON, err := json.Marshal(dict)
 		errorHandle(err, w)
-		result := string(dict_json)
-		fmt.Fprintf(w, result)
-	}
-	if r.Method == "PUT" {
-		tem_goods := Goodsinfo{}
-		dict := dic{}
-		do_return := r.Form["return_goods"][0]
-		userid, _ := strconv.Atoi(r.Form["userid"][0])
-		goods_id := r.Form["goods_id"][0]
-		db.QueryRow("select user_id,is_return form lost_goods where id=?", goods_id).Scan(tem_goods.Userid, tem_goods.Is_return)
-		if userid == tem_goods.Userid && do_return == "1" {
-			stmt, err := db.Prepare("update lost_goods set is_return=? where id=?")
-			errorHandle(err, w)
-			stmt.Exec(1, goods_id)
-			dict.Flag = 1
-			dict.Msg = "归还成功"
-		} else if userid == tem_goods.Userid && tem_goods.Is_return == 0 {
-			dict.Msg = "失物可编辑"
-			dict.Is_edit = 1
-		} else {
-			dict.Msg = "用户无权编辑"
-			dict.Is_edit = 0
-		}
-		dict_json, err := json.Marshal(dict)
-		errorHandle(err, w)
-		result := string(dict_json)
-		fmt.Fprintf(w, result)
-	}
-	if r.Method == "DELETE" {
-		tem_goods := Goodsinfo{}
-		dict := dic{}
-		delete_code := r.Form["delete_code"][0]
-		userid, _ := strconv.Atoi(r.Form["userid"][0])
-		goods_id := r.Form["goods_id"][0]
-		db.QueryRow("select user_id,status form lost_goods where id=?", goods_id).Scan(tem_goods.Userid, tem_goods.Status)
-		if userid == tem_goods.Userid && delete_code == "1" {
-			stmt, err := db.Prepare("update lost_goods set status=? where id=?")
-			errorHandle(err, w)
-			stmt.Exec(0, goods_id)
-			dict.Flag = 1
-			dict.Msg = "删除成功"
-		} else {
-			dict.Msg = "删除失败"
-		}
-		dict_json, err := json.Marshal(dict)
-		errorHandle(err, w)
-		result := string(dict_json)
+		result := string(dictJSON)
 		fmt.Fprintf(w, result)
 	}
 }
@@ -284,36 +272,47 @@ func edit(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	dict := dic{}
-	tem_goods := Goodsinfo{}
+	temGoods := Goodsinfo{}
 	r.ParseForm()
+	var (
+		token     string
+		curUserid int
+		isValid   bool
+		result    string
+	)
 	if r.Method == "GET" {
-		var result string
-		goods_id, _ := strconv.Atoi(r.Form["goods_id"][0])
-		rows := db.QueryRow("select * from lost_goods where id = ?", goods_id)
-		rows.Scan(&tem_goods.Goodsid, &tem_goods.Goodsname, &tem_goods.Address, &tem_goods.Pic, &tem_goods.Phone, &tem_goods.Des, &tem_goods.Userid, &tem_goods.Create_time, &tem_goods.Update_time, &tem_goods.View_count, &tem_goods.Status, &tem_goods.Is_return)
-		dict.Data = append(dict.Data, tem_goods)
-		if tem_goods.Is_return == 1 {
-			dict.Msg = "失物已归还禁止编辑"
+		token = r.Form["token"][0]
+		_, curUserid, isValid = validate(token)
+		if !isValid {
+			dict.Msg = "登录失效，请重新登录"
 			dict.Flag = 0
 		} else {
-			dict.Flag = 1
+			goodsID, _ := strconv.Atoi(r.Form["goods_id"][0])
+			rows := db.QueryRow("select * from lost_goods where id = ?", goodsID)
+			rows.Scan(&temGoods.Goodsid, &temGoods.Goodsname, &temGoods.Address, &temGoods.Pic, &temGoods.Phone, &temGoods.Des, &temGoods.Userid, &temGoods.Create_time, &temGoods.Update_time, &temGoods.View_count, &temGoods.Status, &temGoods.Is_return)
+			dict.Data = append(dict.Data, temGoods)
+			if temGoods.Is_return == 1 {
+				dict.Msg = "失物已归还禁止编辑"
+				dict.Flag = 0
+			} else {
+				dict.Flag = 1
+			}
 		}
-		dict_json, err := json.Marshal(dict)
+		dictJSON, err := json.Marshal(dict)
 		errorHandle(err, w)
-		result = string(dict_json)
+		result = string(dictJSON)
 		fmt.Fprintf(w, result)
-	}
-	if r.Method == "POST" {
+		return
+	} else if r.Method == "POST" && temGoods.Userid == curUserid {
 		var ext string
 		uploadFile, handle, err := r.FormFile("file")
 		errorHandle(err, w)
-		var result string
 		tm := time.Unix(time.Now().Unix(), 0)
-		goods_id, _ := strconv.Atoi(r.Form["goods_id"][0])
-		tem_goods.Address = r.Form["address"][0]
-		tem_goods.Goodsname = r.Form["name"][0]
-		tem_goods.Phone = r.Form["phone"][0]
-		tem_goods.Des = r.Form["des"][0]
+		goodsID, _ := strconv.Atoi(r.Form["goods_id"][0])
+		temGoods.Address = r.Form["address"][0]
+		temGoods.Goodsname = r.Form["name"][0]
+		temGoods.Phone = r.Form["phone"][0]
+		temGoods.Des = r.Form["des"][0]
 		name := strings.Split(handle.Filename, ".")
 		ext = strings.ToLower(name[len(name)-1])
 		fileDir := fmt.Sprintf("./media/file/%v/", tm.Format("2006-01-02"))
@@ -326,37 +325,103 @@ func edit(w http.ResponseWriter, r *http.Request) {
 		io.Copy(saveFile, uploadFile)
 		defer uploadFile.Close()
 		defer saveFile.Close()
-		tem_goods.Pic = filename[1:]
-		stmt, err := db.Prepare("update lost_goods set name=?,address=?,pic=?,phone=?,des=?,update_time=? where id=?")
+		temGoods.Pic = filename[1:]
+		stmt, err := db.Prepare("UPDATE lost_goods SET name=?,address=?,pic=?,phone=?,des=?,update_time=? WHERE id=?")
 		errorHandle(err, w)
-		stmt.Exec(tem_goods.Goodsname, tem_goods.Address, tem_goods.Pic, tem_goods.Phone, tem_goods.Des, tm.Format("2006-01-02 15:04:05"), goods_id)
+		stmt.Exec(temGoods.Goodsname, temGoods.Address, temGoods.Pic, temGoods.Phone, temGoods.Des, tm.Format("2006-01-02 15:04:05"), goodsID)
 		dict.Msg = "修改成功"
 		dict.Flag = 1
 		// 上传图片成功
-		dict_json, err := json.Marshal(dict)
+		dictJSON, err := json.Marshal(dict)
 		errorHandle(err, w)
-		result = string(dict_json)
+		result = string(dictJSON)
 		fmt.Fprintf(w, result)
+		return
+	} else {
+		dict.Msg = "用户无权编辑"
+		dict.Flag = 0
+		dictJSON, err := json.Marshal(dict)
+		errorHandle(err, w)
+		result = string(dictJSON)
+		fmt.Fprintf(w, result)
+		return
 	}
 }
 
 func manage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	tem_goods := Goodsinfo{}
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	temGoods := Goodsinfo{}
 	dict := dic{}
+	var result string
 	r.ParseForm()
-	userid := r.URL.Query()["userid"][0]
-	if r.Method == "GET" {
-		rows, _ := db.Query("select * from lost_goods where status=? and user_id=?", 1, userid)
-		for rows.Next() {
-			rows.Scan(&tem_goods.Goodsid, &tem_goods.Goodsname, &tem_goods.Address, &tem_goods.Pic, &tem_goods.Phone, &tem_goods.Des, &tem_goods.Userid, &tem_goods.Create_time, &tem_goods.Update_time, &tem_goods.View_count, &tem_goods.Status, &tem_goods.Is_return)
-			dict.Data = append(dict.Data, tem_goods)
+	if r.Method == "OPTIONS" {
+		fmt.Fprintf(w, "")
+		return
+	} else {
+		token := r.Form["token"][0]
+		_, curUserid, isValid := validate(token)
+		if !isValid {
+			dict.Msg = "登录失效，请重新登录"
+			dict.Flag = 0
+			dictJSON, err := json.Marshal(dict)
+			errorHandle(err, w)
+			result = string(dictJSON)
+			fmt.Fprintf(w, result)
+			return
+		} else if r.Method == "GET" {
+			rows, _ := db.Query("select * from lost_goods where status=? and user_id=?", 1, curUserid)
+			for rows.Next() {
+				rows.Scan(&temGoods.Goodsid, &temGoods.Goodsname, &temGoods.Address, &temGoods.Pic, &temGoods.Phone, &temGoods.Des, &temGoods.Userid, &temGoods.Create_time, &temGoods.Update_time, &temGoods.View_count, &temGoods.Status, &temGoods.Is_return)
+				dict.Data = append(dict.Data, temGoods)
+			}
+			dictJSON, err := json.Marshal(dict)
+			errorHandle(err, w)
+			result = string(dictJSON)
+			fmt.Fprintf(w, result)
+			return
+		} else if r.Method == "PUT" {
+			goodsID := r.Form["goods_id"][0]
+			doReturn := r.Form["return_code"][0]
+			db.QueryRow("select user_id,is_return from lost_goods where id=?", goodsID).Scan(&temGoods.Userid, &temGoods.Is_return)
+			if curUserid == temGoods.Userid && doReturn == "1" {
+				stmt, err := db.Prepare("update lost_goods set is_return=? where id=?")
+				errorHandle(err, w)
+				stmt.Exec(1, goodsID)
+				dict.Flag = 1
+				dict.Msg = "归还成功"
+			} else if curUserid == temGoods.Userid && temGoods.Is_return == 0 {
+				dict.Msg = "失物可编辑"
+				dict.Is_edit = 1
+			} else {
+				dict.Msg = "用户无权编辑"
+				dict.Is_edit = 0
+			}
+			dictJSON, err := json.Marshal(dict)
+			errorHandle(err, w)
+			result := string(dictJSON)
+			fmt.Fprintf(w, result)
+			return
+		} else if r.Method == "DELETE" {
+			goodsID := r.Form["goods_id"][0]
+			deleteCode := r.Form["delete_code"][0]
+			db.QueryRow("select user_id,status from lost_goods where id=?", goodsID).Scan(&temGoods.Userid, &temGoods.Status)
+			if curUserid == temGoods.Userid && deleteCode == "1" {
+				stmt, err := db.Prepare("update lost_goods set status=? where id=?")
+				errorHandle(err, w)
+				stmt.Exec(0, goodsID)
+				dict.Flag = 1
+				dict.Msg = "删除成功"
+			} else {
+				dict.Msg = "删除失败"
+			}
+			dictJSON, err := json.Marshal(dict)
+			errorHandle(err, w)
+			result := string(dictJSON)
+			fmt.Fprintf(w, result)
+			return
 		}
-		dict_json, err := json.Marshal(dict)
-		errorHandle(err, w)
-		result := string(dict_json)
-		fmt.Fprintf(w, result)
 	}
 }
 
--- web/public_func.go
+++ web/public_func.go
@@ -6,6 +6,9 @@ import (
 	"encoding/hex"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func md5V(str string) string {
@@ -14,8 +17,12 @@ func md5V(str string) string {
 	return hex.EncodeToString(h.Sum(nil))
 }
 
-func enbase(str []byte) string {
-	encodeString := base64.StdEncoding.EncodeToString(str)
+func enbase(str string) string {
+	encodestr := []byte(str)
+	for i := 0; i < len(encodestr); i++ {
+		encodestr[i] += 124
+	}
+	encodeString := base64.StdEncoding.EncodeToString(encodestr)
 	return encodeString
 }
 
@@ -23,6 +30,9 @@ func debase(str string) string {
 	decodeBytes, err := base64.StdEncoding.DecodeString(str)
 	if err != nil {
 		log.Fatalln(err)
+	}
+	for i := 0; i < len(decodeBytes); i++ {
+		decodeBytes[i] -= 124
 	}
 	return string(decodeBytes)
 }
@@ -32,3 +42,27 @@ func errorHandle(err error, w http.ResponseWriter) {
 		w.Write([]byte(err.Error()))
 	}
 }
+
+func validate(str string) (username string, userid int, is_valid bool) {
+	content := strings.Split(debase(str), ",")
+	token_time, _ := strconv.ParseInt(content[len(content)-1], 10, 64)
+	cur_time := time.Now().Unix()
+	if cur_time-token_time >= 7200 {
+		is_valid = false
+	} else {
+		is_valid = true
+	}
+	username = content[0]
+	userid, _ = strconv.Atoi(content[1])
+	return username, userid, is_valid
+}
+
+// func main() {
+// 	tm1 := time.Now().Unix()
+// 	input := enbase("hello")
+// 	fmt.Println(input)
+// 	output := debase(input)
+// 	fmt.Println(output)
+// 	tm2 := time.Now().Unix()
+// 	fmt.Println("间隔", tm2+100-tm1)
+// }
